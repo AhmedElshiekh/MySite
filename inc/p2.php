4e591ea8c9f7ab14489980c2db756e4eed8cd18c
ahmed elshiekh <ahmedel.sh50@gmail.com>
2020-02-12 01:03:52 +0200

    Edit icon on skill page and rearrang it

--- inc/p2.php
+++ inc/p2.php
@@ -4,25 +4,33 @@
     <section class="skill">
       <span class="title"><i class="fas fa-clipboard-list"></i> My <?=$title;?></span><br>
       <?php 
-        $linux=array("OS/linux","70","fab fa-linux");
-        $PostgreSQL=array("PostgreSQL","30","fas fa-database");
-        $flutter=array("flutter","50","fab fa-android");
-        $javaScript=array("javaScript","30","fab fa-js");
-        $laravel=array("laravel","50","fab fa-laravel");
-        $php=array("php","70","fab fa-php");
-        $c=array("c/c++","60","fab fa-codiepie");
-        $python=array("python","50","fab fa-python");
-        $bootstrap=array("bootstrap","60","fab fa-bootstrap");
-        $jquery=array("jquery","30","fab fa-quinscape");
-        $uikit=array("uikit","30","fab fa-uikit");
-        $html=array("html/css<br/>sass","90","fab fa-html5");
-        $mysql=array("mysql","50","fas fa-database");
+      // Skills add name , rate and icons As array
+        $linux=       array("OS/linux","70","fab fa-linux");
+        $javaScript=  array("javaScript","30","fab fa-js");
+        $python=      array("python","50","fab fa-python");
+        $bootstrap=   array("bootstrap","70","fab fa-bootstrap");
+        $uikit=       array("uikit","50","fab fa-uikit");
+        $html=        array("html/xml","90","fab fa-html5");
+        $css=        array("css/sass","90","fab fa-css3-alt");
+        $mysql=       array("mysql","50","fas fa-database");
+        $vcs=         array("VCS","60","fab fa-git-alt");
+        // used image icon >>>
+        $laravel=     array("laravel","50","NO","laravel.png");
+        $PostgreSQL=  array("PostgreSql","30","NO","postgresql.png");
+        $jquery=      array("jquery","60","NO","jquery.png");
+        $flutter=     array("flutter","50","NO","flutter.png"); 
+        $kotlin=     array("kotlin","30","NO","kotlin.png"); 
+        $c=           array("c/c++","60","NO","cpp.png");
+        $php=         array("php","70","NO","php.png");
+        $Qt=          array("Qt","30","NO","qt.png");
         // $=array("","","");
-        $var =array($linux,$PostgreSQL,$flutter,$javaScript,$laravel,$php,$c,$python,$bootstrap,$jquery,$uikit,$html,$mysql);
+        // Add all on array [$var] To Show and Arranging it
+        $var =array($linux, $vcs, $PostgreSQL, $mysql, $laravel, $php, $javaScript, $flutter, $kotlin, $python, $c, $Qt, $html, $css, $uikit, $jquery, $bootstrap,);
+        // Loop show arrays
         for ($x = 0; $x<count($var); $x++): 
         $t=$var[$x];?>
           <div class="col-md-6 col-lg-4" >
-            <div class="circle-wrap p<?=$t[1];?>" >
+            <div class="circle-wrap p<?=$t[1]?>" >
               <div class="circle">
                 <div class="mask full">
                   <div class="fill"></div>
@@ -30,10 +38,18 @@
                 <div class="mask half">
                   <div class="fill"></div>
                 </div>
-                <div class="inside-circle"> <?=$t[1];?>% </div>
+                <div class="inside-circle"> <?=$t[1]?>% </div>
               </div>
             </div>
-            <div><h3 style="font-size:3.4vh;"><i class="<?=$t[2];?>"></i> <?=$t[0];?></h3></div>
+            <div>
+              <h3 style="font-size:30;">
+                <?php if ( $t[2] == "NO" ){ ?>
+                    <img src="img/<?=$t[3]?>" width="30" >
+                <?php }else{ ?>
+                    <i class="<?=$t[2]?>"></i> 
+                <?php }; echo $t[0];?>
+              </h3>
+            </div>
           </div>
       <?php endfor;?>
     </section>
